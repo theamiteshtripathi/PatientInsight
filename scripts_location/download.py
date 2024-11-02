faa530bb191d6f6f82ffc705121d0e686ee7bc71
Pappuru-Dinesh <36697509+Pappuru-Dinesh@users.noreply.github.com>
2024-11-02 01:06:26 -0400

    Required changes
    All changes which are necessary
    - Proper folder structure
    - Working airflow mechanisms
    - Script for downloading data correction
    - Script for preprocess data correction

--- scripts_location/download.py
+++ scripts_location/download.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 import requests
 from datasets import load_dataset
@@ -6,23 +7,34 @@ from datasets import load_dataset
 # Load environment variables
 load_dotenv()
 
+
 # Access AWS credentials
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 def download_pmc_patients_dataset(output_dir):
+    print('Function call')
+    
+    # root path
+    root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    print("root path: ", root_path)
+    output_dir = os.path.join(root_path, "data/raw")
+    
+    # Check directories
+    os.makedirs(output_dir, exist_ok=True)
+
     url = "https://huggingface.co/datasets/zhengyun21/PMC-Patients/resolve/main/PMC-Patients.csv"
     output_path = os.path.join(output_dir, "PMC-Patients.csv")
-    
     response = requests.get(url)
     response.raise_for_status()
     
+    print("Downloading dataset")
     with open(output_path, "wb") as f:
         f.write(response.content)
     
     print(f"Dataset downloaded to {output_path}")
 
 if __name__ == "__main__":
+    print("Start script")
     output_dir = "data/raw"
-    os.makedirs(output_dir, exist_ok=True)
     download_pmc_patients_dataset(output_dir)
