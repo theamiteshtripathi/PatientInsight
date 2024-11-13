370f33e943e8fd7f9c85950c71161b3722b39316
deepaku23 <deepakuday23@gmail.com>
2024-11-13 02:21:17 -0500

    retrieval of relevant documents based on a query

---
+++ backend/models/RAG/src/retriever.py
@@ -0,0 +1,10 @@
+from backend.config.config import Config
+print(Config.N_RESULTS)
+
+class Retriever:
+    def __init__(self, collection):
+        self.collection = collection
+
+    def retrieve(self, query, n_results=Config.N_RESULTS):
+        results = self.collection.query(query_texts=[query], n_results=n_results)
+        return [doc for doc in results['documents'][0]]
