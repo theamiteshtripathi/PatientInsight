b45d723eb8dc4f2f3f6ec0e09eb334fa1d685a30
deepaku23 <deepakuday23@gmail.com>
2024-11-15 22:32:08 -0500

    Removed directory from config

--- backend/config/config.py
+++ backend/config/config.py
@@ -6,7 +6,6 @@ load_dotenv()
 class Config:
     # DAG
     DATA_PATH = "zhengyun21/PMC-Patients"
-    PERSIST_DIRECTORY = "/Users/deepakuday/Northeastern/MLOps/Project/PatientInsight/backend/models/RAG/chroma_db"
     EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
     GENERATIVE_MODEL_NAME = "gpt-4o-mini"
     COLLECTION_NAME = "Patient_insight"
