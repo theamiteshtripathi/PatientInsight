5e1901c83534d6ec63f09230c6cbf4510c77b327
Pappuru-Dinesh <36697509+Pappuru-Dinesh@users.noreply.github.com>
2024-11-28 17:44:09 -0500

    fixing data pipeline actions
    Testing actions

--- backend/data_pipeline/tests/test_data_download.py
+++ backend/data_pipeline/tests/test_data_download.py
@@ -21,6 +21,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 # Unit tests
 class TestDataDownload(unittest.TestCase):
     def test_download_pmc_patients_dataset(self):
+        print("Start tests")
         output_dir = "backend/data_pipeline/data/raw/"
         download_pmc_patients_dataset(output_dir)
         self.assertTrue(os.path.exists(os.path.join(root_path, output_dir, "PMC-Patients.csv")))
