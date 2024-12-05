8e29b98d39f1309b8e5b760f8e5f7e522531cf4c
Pappuru-Dinesh <36697509+Pappuru-Dinesh@users.noreply.github.com>
2024-12-05 01:46:28 -0500

    updating base url
    pointing to backend server

--- frontend/src/config/api.config.js
+++ frontend/src/config/api.config.js
@@ -9,7 +9,7 @@
 // export default API_CONFIG; 
 
 const API_CONFIG = {
-    BASE_URL: 'http://k8s-default-backends-3d835ba603-ad3edaa62e54a151.elb.us-east-2.amazonaws.com',
+    BASE_URL: 'http://k8s-default-backends-fc99b5e612-adfcb23dcde39b3b.elb.us-east-2.amazonaws.com',
     ENDPOINTS: {
         CHAT_START: '/api/chat/message',
         CHAT_MESSAGE: '/api/chat/message'
