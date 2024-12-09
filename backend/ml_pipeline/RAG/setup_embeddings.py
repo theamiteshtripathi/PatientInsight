3615099cf09114c7beb33efd130d33924897697b
Pappuru-Dinesh <36697509+Pappuru-Dinesh@users.noreply.github.com>
2024-12-08 21:12:24 -0500

    Adding lambda function
    Final lambda to airflow

--- backend/ml_pipeline/RAG/setup_embeddings.py
+++ backend/ml_pipeline/RAG/setup_embeddings.py
@@ -1,3 +1,11 @@
+import os
+import sys
+from pathlib import Path
+
+# Add the project root directory to the Python path
+root_path = Path(__file__).parent.parent.parent.parent.absolute()
+sys.path.append(str(root_path))
+
 from backend.ml_pipeline.RAG.embeddings import EmbeddingsHandler
 print("working")
 def setup():
